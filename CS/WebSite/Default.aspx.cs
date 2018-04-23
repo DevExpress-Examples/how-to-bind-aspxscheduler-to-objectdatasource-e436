3fad36934fcd48a183499dc5610295779c8e962d
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WebSite/Default.aspx.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Collections;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+using DevExpress.XtraScheduler;
+using DevExpress.Web.ASPxScheduler;
+
+public partial class Default : System.Web.UI.Page
+{
+    ASPxSchedulerStorage Storage { get { return ASPxScheduler1.Storage; } }
+    Object lastInsertedAppointmentId;
+
+
+    protected void Page_Load(object sender, EventArgs e) {
+        SetupMappings();
+        ResourceFiller.FillResources(this.ASPxScheduler1.Storage, 3);
+
+        ASPxScheduler1.AppointmentDataSource = appointmentDataSource;
+        ASPxScheduler1.DataBind();
+
+        ASPxScheduler1.GroupType = SchedulerGroupType.Resource;
+    }
+    void SetupMappings() {
+        ASPxAppointmentMappingInfo mappings = Storage.Appointments.Mappings;
+        Storage.BeginUpdate();
+        try {
+            mappings.AppointmentId = "Id";
+            mappings.Start = "StartTime";
+            mappings.End = "EndTime";
+            mappings.Subject = "Subject";
+            mappings.AllDay = "AllDay";
+            mappings.Description = "Description";
+            mappings.Label = "Label";
+            mappings.Location = "Location";
+            mappings.RecurrenceInfo = "RecurrenceInfo";
+            mappings.ReminderInfo = "ReminderInfo";
+            mappings.ResourceId = "OwnerId";
+            mappings.Status = "Status";
+            mappings.Type = "EventType";
+        }
+        finally {
+            Storage.EndUpdate();
+        }
+    }
+    //Initilazing ObjectDataSource
+    protected void appointmentsDataSource_ObjectCreated(object sender, ObjectDataSourceEventArgs e) {
+        e.ObjectInstance = new CustomEventDataSource(GetCustomEvents());
+    }
+    CustomEventList GetCustomEvents() {
+        CustomEventList events = Session["ListBoundModeObjects"] as CustomEventList;
+        if (events == null) {
+            events = new CustomEventList();
+            Session["ListBoundModeObjects"] = events;
+        }
+        return events;
+    }
+
+    // User generated appointment id    
+    protected void ASPxScheduler1_AppointmentInserted(object sender, PersistentObjectsEventArgs e) {
+        SetAppointmentId(sender, e);
+    }
+
+    protected void appointmentsDataSource_Inserted(object sender, ObjectDataSourceStatusEventArgs e) {
+        this.lastInsertedAppointmentId = e.ReturnValue;
+    }
+    void SetAppointmentId(object sender, PersistentObjectsEventArgs e) {
+        ASPxSchedulerStorage storage = (ASPxSchedulerStorage)sender;
+        Appointment apt = (Appointment)e.Objects[0];
+        storage.SetAppointmentId(apt, this.lastInsertedAppointmentId);
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,12 @@
+# How to bind ASPxScheduler to ObjectDataSource
+
+
+<p>In this example, the ObjectDataSource control is used to bind the ASPxScheduler control to middle-layer business objects. Further, you can implement your own custom CRUD-operations (Create, Read, Update, Delete – a common abbreviation for data operations) using an arbitrary data provider.</p><p>To make the project simple and easy-to-use, data is written into a session.</p><p><strong>See Also:</strong><br />
+<a href="https://www.devexpress.com/Support/Center/p/K18043">How to bind ASPxScheduler to ObjectDataSource</a><br />
+<a href="https://www.devexpress.com/Support/Center/p/E215">How to bind ASPxScheduler to MS SQL Server database</a><br />
+<a href="https://www.devexpress.com/Support/Center/p/E261">How to bind ASPxScheduler to XPO via the Unit of Work</a><br />
+<a href="https://www.devexpress.com/Support/Center/p/E409">How to bind ASPxScheduler to SyBase ASE 15 database</a></p>
+
+<br/>
+
+
